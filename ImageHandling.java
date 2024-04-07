9b1d504a55d265f42431b43dc0df60137bd39b22
Chuicks <eyzhang@ucsd.edu>
2024-04-07 13:18:39 -0700

    finished read method

--- ImageHandling.java
+++ ImageHandling.java
@@ -5,23 +5,31 @@ import javax.imageio.ImageIO;
 
 public class ImageHandling{
     public static void main (String[] args) throws IOException{
+        //customize image size
         int width = 800;
         int height = 800;
         BufferedImage image = null;
-        File imageFile = new File("\test_image.png");
-        image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
-        //image = ImageIO.read(imageFile);
-        //image = readFromFile(width, height, image);
-        //writeToFile(image);
+        image = readFromFile(width, height, image);
+        writeToFile(image);
     }
 
-    /* 
     private static BufferedImage readFromFile(int width, int height, BufferedImage image) throws IOException{
-        //can customize the name of the image that we are converting
-        File imageFile = new File("\test_image.png");
+        //can customize the name of the image file that we are converting
+        File imageFile = new File("./test_image.png");
         image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
         image = ImageIO.read(imageFile);
         return image;
     }
-    */
+
+    private static void writeToFile(BufferedImage image) throws IOException{
+        try{
+            File output = new File("./ASCII_out.txt");
+            output.createNewFile();
+            ImageIO.write(image, "txt",output);
+            System.out.println("writing complete");
+        }
+        catch (IOException e){
+            System.out.println("Error: " + e);
+        }
+    }
 }
