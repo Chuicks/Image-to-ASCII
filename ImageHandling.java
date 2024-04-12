f47072e3f3b1ca55ed6ea9021dd898fa3ae486ce
Chuicks <eyzhang@ucsd.edu>
2024-04-11 21:40:11 -0700

    Finished implementation of project

--- ImageHandling.java
+++ ImageHandling.java
@@ -1,13 +1,15 @@
 import java.io.IOException;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileWriter;
 import javax.imageio.ImageIO;
 
+
 public class ImageHandling{
+    //customize image size
+    public static int width = 800;
+    public static int height = 800;
     public static void main (String[] args) throws IOException{
-        //customize image size
-        int width = 800;
-        int height = 800;
         BufferedImage image = null;
         image = readFromFile(width, height, image);
         writeToFile(image);
@@ -25,7 +27,53 @@ public class ImageHandling{
         try{
             File output = new File("./ASCII_out.txt");
             output.createNewFile();
-            ImageIO.write(image, "txt",output);
+            FileWriter myWriter = new FileWriter("ASCII_out.txt");
+            for(int i = 0; i < width; i++){        
+                String input= "";
+                for(int j = 0; j < height; j++){
+                    int color = image.getRGB(j,i);
+                    int blue = color & 0xff;
+                    int green = (color & 0xff00) >> 8;
+                    int red = (color & 0xff0000) >> 16;
+                    int color_sum = blue + green + red;
+
+                    //assign ASCII character to pixel
+                    System.out.print(input);
+                    if (color_sum == 0){
+                        input += "#";
+                    }
+                    else if(color_sum > 0 && color_sum < 100){
+                        input += "X";
+                    }
+                    else if(color_sum >= 100 && color_sum < 200){
+                        input += "%";
+                    }
+                    else if(color_sum >= 200 && color_sum < 300){
+                        input += "&";
+                    }
+                    else if(color_sum >= 300 && color_sum < 400){
+                        input += "*";
+                    }
+                    else if(color_sum >= 400 && color_sum < 500){
+                        input += "+";
+                    }
+                    else if(color_sum >= 500 && color_sum < 600){
+                        input += "/";
+                    }
+                    else if(color_sum >= 600 && color_sum < 700){
+                        input += "(";
+                    }
+                    else if(color_sum >= 700 && color_sum < 755){
+                        input += "'";
+                    }
+                    else{
+                        input += " ";
+                    }
+                }
+                input += "\n";
+                myWriter.write(input);
+            }
+            myWriter.close();
             System.out.println("writing complete");
         }
         catch (IOException e){
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Image to ASCII
- 
+Translates any image to ASCII characters and outputs it as a txt file.
+Can change the input image size and also the file type. (Currently using a 800x800 png)
+Project idea inspired by https://youtu.be/jAMegKEetx4?si=-aKNV9rFpQCIob4X.
